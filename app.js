08b1a68a04a43f46bf963fc9b1837b1e24782ce5
Rustam Galin <galinrkh@gmail.com>
2021-11-30 02:45:51 +0500

    refactor: use BEM notation according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -24,17 +24,17 @@ var createNewTaskElement=function(taskString){
     var checkBox=document.createElement("input");//checkbx
     checkBox.classList.add('input');
     checkBox.classList.add('input-checkbox');
-    checkBox.classList.add('li-item');
+    checkBox.classList.add('li__item');
     //label
     var label=document.createElement("label");//label
     //input (text)
     var editInput=document.createElement("input");//text
     editInput.classList.add('input');
-    editInput.classList.add('li-item');
+    editInput.classList.add('li__item');
     //button.edit
     var editButton=document.createElement("button");//edit button
     editButton.classList.add('button');
-    editButton.classList.add('li-item');
+    editButton.classList.add('li__item');
 
     //button.delete
     var deleteButton=document.createElement("button");//delete button
@@ -45,8 +45,8 @@ var createNewTaskElement=function(taskString){
 
     label.innerText=taskString;
     label.className='task';
-    label.classList.add('li-label');
-    label.classList.add('li-item');
+    label.classList.add('li__label');
+    label.classList.add('li__item');
 
     //Each elements, needs appending
     checkBox.type="checkbox";
@@ -54,17 +54,17 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
     editInput.classList.add('input');
     editInput.classList.add('input-text');
-    editInput.classList.add('li-input-text');
-    editInput.classList.add('li-item');
+    editInput.classList.add('li__input-text');
+    editInput.classList.add('li__item');
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
     editButton.classList.add('button');
-    editButton.classList.add('li-item');
+    editButton.classList.add('li__item');
 
     deleteButton.className="delete";
     deleteButton.classList.add('button');
-    deleteButton.classList.add('li-item');
+    deleteButton.classList.add('li__item');
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -150,12 +150,13 @@ var taskCompleted=function(){
 
 var taskIncomplete=function(){
     console.log("Incomplete Task...");
-//Mark task as incomplete.
+    //Mark task as incomplete.
     //When the checkbox is unchecked
     //Append the task list item to the #incomplete-tasks.
     var listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
+
 }
 
 
